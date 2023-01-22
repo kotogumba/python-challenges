e190964d2373b51609b3f38e892e635f3adfefdf
Dmytro Plechun <112661996+kotogumba@users.noreply.github.com>
2023-01-22 14:58:16 +0200

    Create two_sum.py

---
+++ two sum/two_sum.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+    # Initialize an empty dictionary
+    num_indices = {}
+    # Iterate through the array
+    for i, num in enumerate(nums):
+        # Check if the target minus the current element exists in the dictionary
+        if target - num in num_indices:
+            # If it does, return the indices
+            return [num_indices[target - num], i]
+        # If it doesn't, add the current element's index to the dictionary
+        num_indices[num] = i
